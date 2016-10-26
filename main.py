fc61295917c19915db85c40294b16cb43c05c395
John Stauffer <jstauffer@homeadvisor.com>
2016-10-26 15:23:05 -0600

    Tweets are written directly to file & pie chart of hashtags

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Import the necessary methods from tweepy library
+# Import the necessary methods from tweepy library
 from tweepy import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -9,22 +9,26 @@ access_token_secret = "i3SKuFgtGHLuCLQWo8x1tlapqDFFuY2rbXNCuWovipK1e"
 consumer_key = "F9S3jrxWl3gpRkZYdzqrzzliA"
 consumer_secret = "Ckf5ScaUVkz8QtHr1AD5r9HAkXnhm4ED9AsMBy52DOgXiV5jEe"
 
-filter_keys = ['#imwithher']
+# This is list of words that will filter the twitter stream
+filter_keys = ['#imwithher', 'hillary2016', 'trump2016', 'votetrump']
+
+# Setting up file for tweets to be stored
+twitter_data_path = 'data/twitter_data_1.txt'
+twitter_data_store = open(twitter_data_path, 'w+')
+
 
 # Print to StdOut
 class StdOutListener(StreamListener):
-
     def on_data(self, data):
-        print (data)
+        twitter_data_store.write(data)
         return True
 
     def on_error(self, status):
-        print (status)
+        print(status)
 
 
 if __name__ == '__main__':
-
-    #Twitter api authentication
+    # Twitter api authentication
     l = StdOutListener()
 
     # This handles authentication
@@ -32,7 +36,5 @@ if __name__ == '__main__':
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
 
-    #Filter stream of tweets with list of keywords
+    # Filter stream of tweets with list of keywords
     stream.filter(track=filter_keys)
-
-
--- parser.py
+++ parser.py
@@ -1,9 +1,11 @@
 import json
 import pandas
+import re
 import matplotlib.pyplot as plt
+from textblob import TextBlob as tb
 
 # File for response
-tweets_data_path = 'data/twitter_data_el16.txt'
+tweets_data_path = 'data/twitter_data_1.txt'
 columns = 5
 
 tweets_data = []
@@ -15,16 +17,71 @@ for line in tweets_file:
     except:
         continue
 
-print(len(tweets_data))
+
+# Check for word in string passed in
+def word_in_text(word, text):
+    word = word.lower()
+    text = text.lower()
+    match = re.search(word, text)
+    if match:
+        return True
+    return False
+
+
+# Auto percentage with support for showing values in pie chart
+def make_autopct(values):
+    def my_autopct(pct):
+        total = sum(values)
+        val = int(round(pct * total / 100.0))
+        return '{p:.2f}%  ({v:d})'.format(p=pct, v=val)
+
+    return my_autopct
+
 
 # create DataFrame to simplify manipulation
 tweets = pandas.DataFrame()
-# adding columns to DataFrame
-tweets['country'] = list(map(lambda tweet: tweet['place']['full_name'] if tweet['place'] != None else None, tweets_data))
 
-# creating char of location
+# adding columns to DataFrame
+tweets['country'] = list(
+    map(lambda tweet: tweet['place']['full_name'] if tweet['place'] != None else None, tweets_data))
+tweets['lang'] = list(map(lambda tweet: tweet['lang'], tweets_data))
+tweets['text'] = list(map(lambda tweet: tweet['text'], tweets_data))
+
+tweets['imwithher'] = tweets['text'].apply(lambda tweet: word_in_text('#imwithher', tweet))
+tweets['hillary2016'] = tweets['text'].apply(lambda tweet: word_in_text('#hillary2016', tweet))
+tweets['trump2016'] = tweets['text'].apply(lambda tweet: word_in_text('#trump2016', tweet))
+tweets['votetrump'] = tweets['text'].apply(lambda tweet: word_in_text('#votetrump', tweet))
+
+# Creating pie chart of hashtags
+hashtags = ['#imwithher', '#hillary2016', '#trump2016', '#votetrump']
+tweets_by_hashtag = [tweets['imwithher'].value_counts()[True],
+                     tweets['hillary2016'].value_counts()[True],
+                     tweets['trump2016'].value_counts()[True],
+                     tweets['votetrump'].value_counts()[True]]
+
+colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+explode = (0.1, 0, 0, 0)  # explode 1st slice
+
+# Setting axis labels and ticks
+plt.pie(tweets_by_hashtag, explode=explode, labels=hashtags, colors=colors,
+        autopct=make_autopct(tweets_by_hashtag), shadow=True, startangle=140)
+plt.axis('equal')
+plt.show()
+
+# creating chart of location
 tweets_by_location = tweets['country'].value_counts()
 print(tweets_by_location)
+
+fig, ax = plt.subplots()
+ax.tick_params(axis='x', labelsize=15)
+ax.tick_params(axis='y', labelsize=10)
+ax.set_xlabel('Location', fontsize=15)
+ax.set_ylabel('Number of tweets', fontsize=15)
+ax.set_title('Top 5 locations', fontsize=15, fontweight='bold')
+tweets_by_location[:columns].plot(ax=ax, kind='bar', color='red')
+
+# creating chart of languages
+tweets_by_language = tweets['lang'].value_counts()
 
 fig, ax = plt.subplots()
 ax.tick_params(axis='x', labelsize=15)
@@ -32,6 +89,6 @@ ax.tick_params(axis='y', labelsize=10)
 ax.set_xlabel('Languages', fontsize=15)
 ax.set_ylabel('Number of tweets', fontsize=15)
 ax.set_title('Top 5 locations', fontsize=15, fontweight='bold')
-tweets_by_location[:columns].plot(ax=ax, kind='bar', color='red')
+tweets_by_language[:columns].plot(ax=ax, kind='bar', color='blue')
 
 input("Press enter to close program")
